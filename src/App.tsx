f84e26d370f1088115337671c23e544d663be920
aaron oster <bigaaron@udel.edu>
2023-04-11 16:13:33 -0400

    Manned up and created our beginning website

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App(): JSX.Element {
+    return (
+        <div className="App">
+            <header className="App-header">
+                UD CISC275 with React Hooks and TypeScript
+            </header>
+            <h1>man up</h1>
+        </div>
+    );
+}
+
+export default App;

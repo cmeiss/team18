74adf09ca6e0f3272066c69991a7af6eaba4bfe0
aaron oster <bigaaron@udel.edu>
2023-04-11 16:49:20 -0400

    ,

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                UD CISC275 with React Hooks and TypeScript,
             </header>
             <h1>man up</h1>
         </div>

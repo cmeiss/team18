85c8f993acd1abcf6189346a547150189b2362d9
Cornelia <cmeiss@udel.edu>
2023-04-13 18:06:28 -0400

    displayTask shows all task attributes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
 import React from "react";
+import { Task } from "../interfaces/task";
 import "./App.css";
+import { DisplayTask } from "./DisplayTask";
+
+const TASK: Task =
+    //this is a completely random test task to get something displayed, needs to be deleted once we have an actual task list
+    {
+        name: "test",
+        description: "abc",
+        status: false,
+        image: "picture",
+        steps: ["a", "b", "c"],
+        difficulty: 3,
+        numUsers: 2,
+        time: 1345
+    };
 
 function App(): JSX.Element {
     return (
@@ -12,6 +27,16 @@ function App(): JSX.Element {
                 Team Members: Sydni Wright, Kaitlyn Sullivan, Aaron Oster, Will
                 Sharp, Cornelia Meiss
             </div>
+            <DisplayTask
+                name={TASK.name}
+                description={TASK.description}
+                status={TASK.status}
+                image={TASK.image}
+                steps={TASK.steps}
+                difficulty={TASK.difficulty}
+                numUsers={TASK.numUsers}
+                time={TASK.time}
+            ></DisplayTask>
         </div>
     );
 }
---
+++ src/DisplayTask.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import { Task } from "../interfaces/task";
+import TASK from "../src/App";
+
+interface taskProps {
+    name: string;
+    description: string;
+    status: boolean;
+    image: string;
+    steps: string[];
+    difficulty: number;
+    numUsers: number;
+    time: number;
+}
+
+export function DisplayTask(task: taskProps): JSX.Element {
+    return (
+        <div>
+            <h3>Task: {task.name}</h3>
+            <div>{task.description}</div>
+            <div>
+                {task.status ? "Done" : "ToDo"}
+                {"     "} difficulty: {task.difficulty}
+                {"     "}
+                do at {task.time}
+            </div>
+            <div>{task.steps}</div>
+            <div>{task.numUsers}</div>
+            <div>{task.image}</div>
+        </div>
+    );
+}

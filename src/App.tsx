8efb48cd770652216810b50b9e0bfb5228226168
Cornelia <cmeiss@udel.edu>
2023-04-14 10:50:55 -0400

    Added FinalProject to header to get new deployment

--- src/App.tsx
+++ src/App.tsx
@@ -34,9 +34,9 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript,
+                UD CISC275 with React Hooks and TypeScript, Final Project
             </header>
-            <h1>man up</h1>
+            <h1>man up </h1>
             <div>
                 Team Members: Sydni Wright, Kaitlyn Sullivan, Aaron Oster, Will
                 Sharp, Cornelia Meiss

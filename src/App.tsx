b125e0140d436f614303f017768ec8b5e4ec18e4
aaron oster <bigaaron@udel.edu>
2023-04-16 19:24:02 -0400

    created Form to add users delete users and see current users in dropdown list

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { ChangeRole } from "./selectuser";
 
 function App(): JSX.Element {
     return (
@@ -8,6 +9,7 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript,
             </header>
             <h1>man up</h1>
+            <ChangeRole></ChangeRole>
         </div>
     );
 }

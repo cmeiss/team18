6e5f179cbe57e60f6a628860ed1c0edba7925e4b
aaron oster <bigaaron@udel.edu>
2023-04-16 22:34:40 -0400

    changed roles from list of string to list of users that is defined as an interface

--- src/selectuser.tsx
+++ src/selectuser.tsx
@@ -1,20 +1,26 @@
 import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
+import { User } from "./interfaces/user";
 //this is a function that returns a jsx element in our app.tsx file
 interface ChangeRoleProperties {
-    Role: string;
-    roles: string[];
+    Role: User;
+    roles: User[];
 }
 
 export function ChangeRole(ChangeRoleProps: ChangeRoleProperties): JSX.Element {
     // creating 4 states here that will be used by our components to add and delete users
-    const [Role, setRole] = useState<string>(ChangeRoleProps.Role); //Starting role default set to user
-    const [roles, setRoles] = useState<string[]>(ChangeRoleProps.roles); // these are original users these can be changed
+    const [Role, setRole] = useState<User>(ChangeRoleProps.Role); //Starting role default set to user
+    const [roles, setRoles] = useState<User[]>(ChangeRoleProps.roles); // these are original users these can be changed
     const [editmode, seteditmode] = useState<boolean>(false); //whether the textbox will appear boolean
     const [newUser, setNewUser] = useState<string>("User2"); //the value currently in the text box of edit mode
     // function that sets role based on the role clicked
     function updateRole(event: React.ChangeEvent<HTMLSelectElement>) {
-        setRole(event.target.value);
+        const NewRole = roles.find(
+            (role: User) => role.name === event.target.value
+        );
+        if (NewRole) {
+            setRole(NewRole);
+        }
     }
     function updateEditMode(event: React.ChangeEvent<HTMLInputElement>) {
         seteditmode(event.target.checked);
@@ -24,13 +30,13 @@ export function ChangeRole(ChangeRoleProps: ChangeRoleProperties): JSX.Element {
     }
 
     function AddUsersandEditMode() {
-        setRoles([...roles, newUser]);
+        setRoles([...roles, { name: newUser, userList: [] }]);
         seteditmode(false);
     }
     function DeleteUsersandEditMode() {
         setRoles(
-            [...roles].filter((role: string): boolean =>
-                role !== newUser ? true : false
+            [...roles].filter((role: User): boolean =>
+                role.name !== newUser ? true : false
             )
         );
         seteditmode(false);
@@ -41,14 +47,14 @@ export function ChangeRole(ChangeRoleProps: ChangeRoleProperties): JSX.Element {
             <div>
                 <Form.Group controlId="userEmotions">
                     <Form.Label>Select Your Role</Form.Label>
-                    <Form.Select value={Role} onChange={updateRole}>
-                        {roles.map((role: string) => (
+                    <Form.Select value="Roles" onChange={updateRole}>
+                        {roles.map((role: User) => (
                             // eslint-disable-next-line react/jsx-key
-                            <option value={role}>{role}</option>
+                            <option value={role.name}>{role.name}</option>
                         ))}
                     </Form.Select>
                 </Form.Group>
-                The user Chose {Role}
+                The user Chose {Role.name}
             </div>
             <h3>Click Switch to add or delete users</h3>
             <Form.Check

6e6e4586acae45ca83157cee41b1c935de03b0b7
Kaitlyn Sullivan <sullivank210@gmail.com>
2023-04-17 00:56:05 -0400

    Messed up last commit, forgot to save. This includes the stuff from test Task

--- src/TaskFunctions.test.ts
+++ src/TaskFunctions.test.ts
@@ -3,16 +3,17 @@ import { Task } from "./interfaces/task";
 import {
     makeTask,
     addTask,
-    /*deleteTask,*/
+    delTask,
     setName,
     setDescription,
     setStatus,
-    setImage /*, 
-    setSteps, 
-    setDiff, 
-    setNumUsers, 
-    setTime*/
+    setImage,
+    setSteps,
+    setDifficulty,
+    setNumUsers,
+    setTime
 } from "./TaskFunctions";
+import { deleteTask } from "./user-functions";
 
 // Creating a test task to use as a immuatable object in all tests
 const TESTTASK: Task = {
@@ -20,33 +21,53 @@ const TESTTASK: Task = {
     description: "taskfunc test task",
     status: false,
     image: "blank",
-    steps: [],
+    steps: ["i", "am", "immutable"],
     difficulty: 0,
     numUsers: 0,
     time: 0
 };
 
 // Backup task, to ensure changes are immutable
-const BACKUPTASK: Task = TESTTASK;
+const BACKUPTASK: Task = {
+    name: "test",
+    description: "taskfunc test task",
+    status: false,
+    image: "blank",
+    steps: ["i", "am", "immutable"],
+    difficulty: 0,
+    numUsers: 0,
+    time: 0
+};
 
+const STEPS: string[] = ["1", "2", "3"];
+const BACKUPSTEPS: string[] = ["1", "2", "3"];
+
+// some more tasks
+const TASK2: Task = makeTask(
+    "test2",
+    "taskarr ind 1",
+    true,
+    "dog",
+    ["walk", "feed"],
+    1,
+    0,
+    1
+);
+const TASK3: Task = makeTask(
+    "task3",
+    "taskarr ind 2",
+    false,
+    "cat",
+    ["pet", "love", "feed"],
+    3,
+    1,
+    2
+);
 // Immutable task array
-const TASKARR: Task[] = [
-    TESTTASK,
-    makeTask("test2", "taskarr ind 1", true, "dog", ["walk", "feed"], 1, 0, 1),
-    makeTask(
-        "task3",
-        "taskarr ind 2",
-        false,
-        "cat",
-        ["pet", "love", "feed"],
-        3,
-        1,
-        2
-    )
-];
+const TASKARR: Task[] = [TESTTASK, TASK2, TASK3];
 
 // Use to ensure immutable
-const BACKUPTASKARR: Task[] = TASKARR;
+const BACKUPTASKARR: Task[] = [TESTTASK, TASK2, TASK3];
 
 describe("Testing the task functions", () => {
     test("Testing the makeTask function", () => {
@@ -54,6 +75,36 @@ describe("Testing the task functions", () => {
             makeTask("test", "taskfunc test task", false, "blank", [], 0, 0, 0)
         ).toEqual(TESTTASK);
     });
-
-    test("Testing the delete task function"), () => 
+    test("Testing the delTask function", () => {
+        expect(delTask(TESTTASK, TASKARR)).toEqual([TASK2, TASK3]);
+        expect(delTask(TASK2, TASKARR)).toEqual([TESTTASK, TASK3]);
+        expect(delTask(BACKUPTASK, TASKARR)).toEqual([TASKARR]);
+    });
+    test("Testing the setSteps function", () => {
+        expect(setSteps(STEPS, TESTTASK)).toEqual({
+            name: "test",
+            description: "taskfunc test task",
+            status: false,
+            image: "blank",
+            steps: ["1", "2", "3"],
+            difficulty: 0,
+            numUsers: 0,
+            time: 0
+        });
+        expect(setSteps(["3", "2", "1"], TESTTASK)).toEqual({
+            name: "test",
+            description: "taskfunc test task",
+            status: false,
+            image: "blank",
+            steps: ["3", "2", "1"],
+            difficulty: 0,
+            numUsers: 0,
+            time: 0
+        });
+    });
+    afterEach(() => {
+        expect(TESTTASK).toEqual(BACKUPTASK);
+        expect(TASKARR).toEqual(BACKUPTASKARR);
+        expect(STEPS).toEqual(BACKUPSTEPS);
+    });
 });

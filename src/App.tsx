47556a51258d878e827e996590743e53f92ec30f
aaron oster <bigaaron@udel.edu>
2023-04-16 21:47:24 -0400

    changed location of the changerole function

--- src/App.tsx
+++ src/App.tsx
@@ -39,11 +39,11 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript,
             </header>
-            <ChangeRole Role={role} roles={roles}></ChangeRole>
             <div>
                 Team Members: Sydni Wright, Kaitlyn Sullivan, Aaron Oster, Will
                 Sharp, Cornelia Meiss
             </div>
+            <ChangeRole Role={role} roles={roles}></ChangeRole>
             {/*  This div statement exists only to test the DisplayTask component
             and show how it works by displaying the TASK array,
             This div statement needs to be deleted together with the TASK array

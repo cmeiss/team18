031a6ca37cce9a6b6650c5a0d8dd0b7d495a43b3
aaron oster <bigaaron@udel.edu>
2023-04-20 16:25:34 -0400

    created the admin list and only shows if the user is admin

--- src/list-components/adminlist.tsx
+++ src/list-components/adminlist.tsx
@@ -3,40 +3,42 @@ import React, { useState } from "react";
 import { DisplayTask } from "./DisplayTask";
 import { Task } from "../interfaces/task";
 
-interface CentralItemProps {
+interface AdminItemProps {
     tasks: Task[];
     role: string;
 }
 
-export function AdminList({ role, tasks }: CentralItemProps) {
+export function AdminList({ role, tasks }: AdminItemProps) {
     const [AdminTasks, SetAdminTask] = useState<Task[]>(tasks);
     const [Tasks] = useState<Task[]>(tasks);
     const [Role] = useState<string>(role);
 
-    SetAdminTask(
-        Tasks.filter((task: Task): boolean =>
-            task.numUsers < 2 ? true : false
-        )
+    const NewTasks = Tasks.filter((task: Task): boolean =>
+        task.numUsers < 2 ? true : false
     );
-    return (
-        <div className="AdminList">
-            <div className="Admin">
-                <span> Central List </span>
-                {AdminTasks.map((TASK: Task) => (
-                    <DisplayTask
-                        key={5}
-                        name={TASK.name}
-                        description={TASK.description}
-                        status={TASK.status}
-                        image={TASK.image}
-                        steps={TASK.steps}
-                        difficulty={TASK.difficulty}
-                        numUsers={TASK.numUsers}
-                        time={TASK.time}
-                        role={Role}
-                    ></DisplayTask>
-                ))}
+    if (role === "Admin") {
+        return (
+            <div className="AdminList">
+                <div className="Admin">
+                    <span> Central List </span>
+                    {NewTasks.map((TASK: Task) => (
+                        <DisplayTask
+                            key={5}
+                            name={TASK.name}
+                            description={TASK.description}
+                            status={TASK.status}
+                            image={TASK.image}
+                            steps={TASK.steps}
+                            difficulty={TASK.difficulty}
+                            numUsers={TASK.numUsers}
+                            time={TASK.time}
+                            role={Role}
+                        ></DisplayTask>
+                    ))}
+                </div>
             </div>
-        </div>
-    );
+        );
+    } else {
+        return <span>Man UP</span>;
+    }
 }

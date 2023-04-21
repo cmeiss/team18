6c916f1ba18cf03b6e463849dc8edefc94993973
Cornelia <cmeiss@udel.edu>
2023-04-21 15:28:24 -0400

    added task state with five actual tasks

--- src/App.tsx
+++ src/App.tsx
@@ -9,41 +9,44 @@ import { UserList } from "./list-components/UserList";
 import { ChangeRole } from "./list-components/ChangeRole";
 import { ModifyUsers } from "./list-components/ModifyUsers";
 import { AdminList } from "./list-components/adminlist";
+import { TASKS } from "./TASKS";
 
-const TASK: Task[] =
-    //this is a completely random test task array to get something
-    //displayed, needs to be deleted once we have an actual task list
-    [
-        {
-            name: "test",
-            description: "this is the description",
-            status: false,
-            image: "picture",
-            steps: ["a", "b", "c"],
-            difficulty: 3,
-            numUsers: 2,
-            time: 1345
-        },
-        {
-            name: "test2",
-            description: "this is the description",
-            status: false,
-            image: "picture",
-            steps: ["a", "b", "c"],
-            difficulty: 3,
-            numUsers: 1,
-            time: 1345
-        }
-    ];
+// const TASK: Task[] =
+//     //this is a completely random test task array to get something
+//     //displayed, needs to be deleted once we have an actual task list
+//     [
+//         {
+//             name: "test",
+//             description: "this is the description",
+//             status: false,
+//             image: "picture",
+//             steps: ["a", "b", "c"],
+//             difficulty: 3,
+//             numUsers: 2,
+//             time: 1345
+//         },
+//         {
+//             name: "test2",
+//             description: "this is the description",
+//             status: false,
+//             image: "picture",
+//             steps: ["a", "b", "c"],
+//             difficulty: 3,
+//             numUsers: 1,
+//             time: 1345
+//         }
+//     ];
 
 function App(): JSX.Element {
-    const [role, setRole] = useState<User>({ name: "User1", userList: TASK }); //I set this intial user to make the user list display something
+    const [role, setRole] = useState<User>({ name: "User1", userList: TASKS }); //I set this intial user to make the user list display something
     const [roles, setRoles] = useState<User[]>([
         { name: "Please Select: ", userList: [] }, //Please select is necessary because the first item in drop down list is not selectable
         { name: "Super", userList: [] },
         { name: "Admin", userList: [] },
-        { name: "User1", userList: TASK }
+        { name: "User1", userList: TASKS }
     ]); // these are original users these can be changed
+    const [tasks, setTasks] = useState<Task[]>(TASKS);
+
     function updateRole(event: React.ChangeEvent<HTMLSelectElement>) {
         const NewRole = roles.find(
             (role: User) => role.name === event.target.value
@@ -102,11 +105,11 @@ function App(): JSX.Element {
             </div>
             <div className="central">
                 <CentralItemList
-                    tasks={TASK}
+                    tasks={tasks}
                     role={role.name}
                 ></CentralItemList>
                 <div>
-                    <AdminList tasks={TASK} role={role.name}></AdminList>
+                    <AdminList tasks={tasks} role={role.name}></AdminList>
                 </div>
             </div>
         </div>

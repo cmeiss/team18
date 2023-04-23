27fb0e6a0d8a68163f51244338c36a5165e912d0
Kaitlyn Sullivan <sullivank210@gmail.com>
2023-04-23 15:04:38 -0400

    clicking add user leaves edit mode

--- src/list-components/AddUser.test.tsx
+++ src/list-components/AddUser.test.tsx
@@ -26,20 +26,31 @@ describe("Modify user tests", () => {
     });
     test("On switch click, there is a textbox and buttons: ", () => {
         const switchButton = screen.getByRole("checkbox");
+        switchButton.click();
         const addUser = screen.getByRole("button", {
             name: /Add User and Leave Edit Mode/i
         });
         const delUser = screen.getByRole("button", {
             name: /Delete User and Leave Edit Mode/i
         });
-        switchButton.click();
         expect(screen.getByRole("textbox")).toBeInTheDocument();
         expect(addUser).toBeInTheDocument();
         expect(delUser).toBeInTheDocument();
     });
-    test("There is a add user button", () => {
+    test("Clicking add user adds a user and leaves edit mode", () => {
+        // setting up the scenario
         const switchButton = screen.getByRole("checkbox");
         switchButton.click();
-        expect(screen.getByRole("button"));
+        const addUser = screen.getByRole("button", {
+            name: /Add User and Leave Edit Mode/i
+        });
+        const inputUser = screen.getByRole("textbox");
+        userEvent.type(inputUser, "user test");
+        // on add user button click
+        addUser.click();
+        // need help figuring out if it actually adds a user
+        // leaves edit mode: no textbox and no buttons
+        expect(screen.getByRole("textbox")).toBeNull();
+        expect(screen.getByRole("button")).toBeNull();
     });
 });

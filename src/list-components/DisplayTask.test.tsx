3cc2acc08988d783fb8224865ec146197ea47e99
Cornelia <cmeiss@udel.edu>
2023-04-23 16:32:29 -0400

    made out commented tests for displayTask work temporarily

--- src/list-components/DisplayTask.test.tsx
+++ src/list-components/DisplayTask.test.tsx
@@ -1,90 +1,116 @@
-// import React from "react";
-// import { render, screen } from "@testing-library/react";
+import React, { useState } from "react";
+import { render, screen } from "@testing-library/react";
 import { DisplayTask } from "./DisplayTask";
+import { Task } from "../interfaces/task";
 
-// const testTask = {
-//     name: "test",
-//     description: "this is the description",
-//     status: false,
-//     image: "picture",
-//     steps: ["a", "b", "c", "GutenTag", "469476"],
-//     difficulty: 3,
-//     numUsers: 2,
-//     time: 1345
-// };
+const testTask = {
+    name: "test",
+    description: "this is the description",
+    status: false,
+    image: "picture",
+    steps: ["a", "b", "c", "GutenTag", "469476"],
+    difficulty: 3,
+    numUsers: 2,
+    time: 1345
+};
 
-// const testRole1 = "super";
-// const testRole2 = "admin";
+const TaskList = [
+    {
+        name: "test1",
+        description: "description a",
+        status: false,
+        image: "picture",
+        steps: ["a", "b", "c", "GutenTag", "469476"],
+        difficulty: 3,
+        numUsers: 2,
+        time: 1345
+    },
+    {
+        name: "test2",
+        description: "this is the description",
+        status: false,
+        image: "picture",
+        steps: ["a", "b", "c", "GutenTag", "469476"],
+        difficulty: 3,
+        numUsers: 2,
+        time: 1345
+    },
+    {
+        name: "test3",
+        description: "a good description",
+        status: false,
+        image: "picture",
+        steps: ["a", "b", "c", "GutenTag", "469476"],
+        difficulty: 3,
+        numUsers: 2,
+        time: 1345
+    }
+];
 
-// //testing with role being super
-// describe("DisplayView Tests", () => {
-//     beforeEach(() => {
-//         render(
-//             <DisplayTask
-//                 name={testTask.name}
-//                 description={testTask.description}
-//                 status={testTask.status}
-//                 image={testTask.image}
-//                 steps={testTask.steps}
-//                 difficulty={testTask.difficulty}
-//                 numUsers={testTask.numUsers}
-//                 time={testTask.time}
-//                 role={testRole1}
-//             />
-//         );
-//     });
-//     test("Taskname is displayed", () => {
-//         const taskName = screen.getByText(/test/i);
-//         expect(taskName).toBeInTheDocument();
-//     });
-//     test("Num of TaskUsers is there if role is super", () => {
-//         const UserNum = screen.getByText(/Number of Users/i);
-//         expect(UserNum).toBeInTheDocument();
-//     });
-//     test("There is a checkbox", () => {
-//         const doneCheckbox = screen.getByRole("checkbox", {
-//             name: /❌/i || /✔️/i
-//         });
-//         expect(doneCheckbox).toBeInTheDocument();
-//     });
-//     test("Description of the task is displayed", () => {
-//         const desc = screen.getByText(testTask.description);
-//         expect(desc).toBeInTheDocument();
-//     });
-//     test("Steps are displayed", () => {
-//         testTask.steps.every((step) => {
-//             const text = screen.getByText(step);
-//             expect(text).toBeInTheDocument;
-//         });
-//     });
-//     test("difficulty of task is displayed", () => {
-//         const diff = screen.getByText(/Difficulty:/i);
-//         expect(diff).toBeInTheDocument();
-//     });
-//     test("time is displayed", () => {
-//         const time = screen.getByText(/1345/i);
-//         expect(time).toBeInTheDocument();
-//     });
-// });
+const testRole1 = "super";
+const testRole2 = "admin";
+
+const [tasks, setTasks] = useState<Task[]>(TaskList); //this state should not exists but I don't know how to do setTasks without state
+
+//testing with role being super
+describe("DisplayView Tests", () => {
+    beforeEach(() => {
+        render(
+            <DisplayTask
+                task={testTask}
+                tasks={TaskList}
+                updateTasks={setTasks}
+                role={testRole1}
+            />
+        );
+    });
+    test("Taskname is displayed", () => {
+        const taskName = screen.getByText(/test/i);
+        expect(taskName).toBeInTheDocument();
+    });
+    test("Num of TaskUsers is there if role is super", () => {
+        const UserNum = screen.getByText(/Number of Users/i);
+        expect(UserNum).toBeInTheDocument();
+    });
+    test("There is a checkbox", () => {
+        const doneCheckbox = screen.getByRole("checkbox", {
+            name: /❌/i || /✔️/i
+        });
+        expect(doneCheckbox).toBeInTheDocument();
+    });
+    test("Description of the task is displayed", () => {
+        const desc = screen.getByText(testTask.description);
+        expect(desc).toBeInTheDocument();
+    });
+    test("Steps are displayed", () => {
+        testTask.steps.every((step) => {
+            const text = screen.getByText(step);
+            expect(text).toBeInTheDocument;
+        });
+    });
+    test("difficulty of task is displayed", () => {
+        const diff = screen.getByText(/Difficulty:/i);
+        expect(diff).toBeInTheDocument();
+    });
+    test("time is displayed", () => {
+        const time = screen.getByText(/1345/i);
+        expect(time).toBeInTheDocument();
+    });
+});
 
 //testing with role being admin
-// describe("DisplayView Tests", () => {
-//     beforeEach(() => {
-//         render(
-//             <DisplayTask
-//                 name={testTask.name}
-//                 description={testTask.description}
-//                 status={testTask.status}
-//                 image={testTask.image}
-//                 steps={testTask.steps}
-//                 difficulty={testTask.difficulty}
-//                 numUsers={testTask.numUsers}
-//                 time={testTask.time}
-//                 role={testRole2}
-//             />
-//         );
-//     });
-//     test("Num of TaskUsers is not there if role is admin", () => {
-//         expect(screen.queryByText(/Number of User/i)).toBeNull();
-//     });
-// });
+describe("DisplayView Tests", () => {
+    beforeEach(() => {
+        render(
+            <DisplayTask
+                task={testTask}
+                tasks={TaskList}
+                updateTasks={setTasks}
+                role={testRole2}
+            />
+        );
+    });
+    test("Num of TaskUsers is not there if role is admin", () => {
+        expect(screen.queryByText(/Number of User/i)).toBeNull();
+    });
+});

8aae54222847ea47dd3ebe975977e87306dcf504
aaron oster <bigaaron@udel.edu>
2023-04-25 16:21:42 -0400

    manned up and wrote filter by alphabetical order

--- src/list-components/filterlists.tsx
+++ src/list-components/filterlists.tsx
@@ -4,13 +4,21 @@ import { Task } from "../interfaces/task";
 
 //function that filters a list and returns a new list in ascending order based off difficulty
 function filter_by_difficulty(list_of_tasks: Task[]): void {
-    const [new_list_of_tasks, set_new_list_of_tasks] =
-        useState<Task[]>(list_of_tasks);
-
+    //lambda function I created to be passed in the sort
     function mycomparator(a: Task, b: Task) {
         return a.difficulty - b.difficulty;
     }
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    set_new_list_of_tasks(list_of_tasks.sort(mycomparator));
+    //sorts the tasks based on comparison function
+    list_of_tasks.sort(mycomparator);
+}
+function filter_by_alphabetical_order(list_of_tasks: Task[]): void {
+    list_of_tasks.sort(function (a, b) {
+        const nameA = a.name.toLowerCase(),
+            nameB = b.name.toLowerCase();
+        if (nameA < nameB)
+            //sort string ascending
+            return -1;
+        if (nameA > nameB) return 1;
+        return 0; //default return value (no sorting)
+    });
 }

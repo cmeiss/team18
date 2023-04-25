37e57df7f26d2f0c579edf9df4906773fcb26840
sydni wright <sydniw@udel.edu>
2023-04-25 18:47:12 -0400

    sydniw-wq trying to connect edit diffictulty to edit task

--- src/editing-components/edit-difficulty.tsx
+++ src/editing-components/edit-difficulty.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { useState } from "react";
 import { Form } from "react-bootstrap";
 
-export function App(): JSX.Element {
-    //setting state
-    const [difficulty, setDifficulty] = useState<number>(0);
-
+interface diffProps {
+    diff: number;
+    setDifficulty: (newDiff: number) => void;
+}
+export function editDifficulty({ diff, setDifficulty }: diffProps) {
     //update functions
     function updateDifficulty(event: React.ChangeEvent<HTMLInputElement>) {
         setDifficulty(parseInt(event.target.value));
@@ -13,50 +13,56 @@ export function App(): JSX.Element {
 
     return (
         <div className="difficulty-edit">
+            Task Difficulty: {diff}
             <Form.Check
                 inline
                 label="star-1"
                 type="radio"
                 name="difficulty"
+                value="1"
                 onChange={updateDifficulty}
                 id="poor"
-                checked={difficulty === 1}
+                checked={diff === 1}
             />
             <Form.Check
                 inline
                 label="star-2"
                 type="radio"
                 name="difficulty"
+                value="2"
                 onChange={updateDifficulty}
                 id="not good"
-                checked={difficulty === 2}
+                checked={diff === 2}
             />
             <Form.Check
                 inline
                 label="star-3"
                 type="radio"
                 name="difficulty"
+                value="3"
                 onChange={updateDifficulty}
                 id="neutral"
-                checked={difficulty === 3}
+                checked={diff === 3}
             />
             <Form.Check
                 inline
                 label="star-4"
                 type="radio"
                 name="difficulty"
+                value="4"
                 onChange={updateDifficulty}
                 id="pretty good"
-                checked={difficulty === 4}
+                checked={diff === 4}
             />
             <Form.Check
                 inline
                 label="star-5"
                 type="radio"
                 name="difficulty"
+                value="5"
                 onChange={updateDifficulty}
                 id="great"
-                checked={difficulty === 5}
+                checked={diff === 5}
             />
         </div>
     );

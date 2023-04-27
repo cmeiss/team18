6b14456c5d83daf1e64a66fb9c7023260c17a94d
sydni wright <sydniw@udel.edu>
2023-04-27 14:49:57 -0400

    sydniw-wq added test cases to delete task

--- src/list-components/deleteTak.test.tsx
+++ src/list-components/deleteTak.test.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { deleteTask } from "./deleteTask-component";
+import { DeleteTask } from "./deleteTask-component";
 import { Task } from "../interfaces/task";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-const TASK1 = {
+//basic tasks and task lists to test
+const TASK1: Task = {
     name: "task1",
     description: "description",
     status: false,
@@ -14,7 +16,7 @@ const TASK1 = {
     time: 1010
 };
 
-const TASK2 = {
+const TASK2: Task = {
     name: "task2",
     description: "description",
     status: false,
@@ -25,7 +27,7 @@ const TASK2 = {
     time: 1100
 };
 
-const TASK3 = {
+/*const TASK3: Task = {
     name: "task3",
     description: "description",
     status: false,
@@ -34,8 +36,52 @@ const TASK3 = {
     difficulty: 2,
     numUsers: 2,
     time: 1200
-};
+}; */
 
 const TASKLIST1 = [TASK1, TASK2];
-const TASKLIST2 = [TASK3];
-const TASKLIST3 = [TASK1, TASK2, TASK3];
+//const TASKLIST2 = [TASK3];
+//const TASKLIST3 = [TASK1, TASK2, TASK3];
+
+//actual tests
+describe("Delete task tests", () => {
+    beforeEach(() =>
+        render(
+            <DeleteTask
+                item={TASK1}
+                tasks={TASKLIST1}
+                setTasks={
+                    function (/*newTasks: Task[]*/): void {
+                        throw new Error("function not implemented");
+                    }
+                }
+            />
+        )
+    );
+    test("There is a switch", () => {
+        const switchButton = screen.getByRole("switch");
+        expect(switchButton).toBeInTheDocument();
+    });
+    test("On switch click, there is a textbox and buttons: ", () => {
+        const switchButton = screen.getByRole("switch");
+        switchButton.click();
+        const deltask1 = screen.getByRole("button", {
+            name: /Delete Task and Leave Edit Mode/i
+        });
+        expect(screen.getByRole("textarea")).toBeInTheDocument();
+        expect(deltask1).toBeInTheDocument();
+    });
+
+    test("Clicking delete task deletes from the list and leaves edit mode", () => {
+        const switchButton = screen.getByRole("switch");
+        switchButton.click();
+        const delTask = screen.getByRole("button", {
+            name: /Delete Task and Leave Edit Mode/i
+        });
+        const inputUser = screen.getByRole("textarea");
+        userEvent.type(inputUser, "task test");
+        // on delete task button click
+        delTask.click();
+        expect(screen.getByRole("textarea")).toBeNull();
+        expect(screen.getByRole("button")).toBeNull();
+    });
+});
--- src/list-components/dist/deleteTak.test.js
+++ src/list-components/dist/deleteTak.test.js
@@ -1,5 +1,10 @@
 "use strict";
 exports.__esModule = true;
+var react_1 = require("react");
+var deleteTask_component_1 = require("./deleteTask-component");
+var react_2 = require("@testing-library/react");
+var user_event_1 = require("@testing-library/user-event");
+//basic tasks and task lists to test
 var TASK1 = {
     name: "task1",
     description: "description",
@@ -20,7 +25,7 @@ var TASK2 = {
     numUsers: 1,
     time: 1100
 };
-var TASK3 = {
+/*const TASK3: Task = {
     name: "task3",
     description: "description",
     status: false,
@@ -29,7 +34,41 @@ var TASK3 = {
     difficulty: 2,
     numUsers: 2,
     time: 1200
-};
+}; */
 var TASKLIST1 = [TASK1, TASK2];
-var TASKLIST2 = [TASK3];
-var TASKLIST3 = [TASK1, TASK2, TASK3];
+//const TASKLIST2 = [TASK3];
+//const TASKLIST3 = [TASK1, TASK2, TASK3];
+//actual tests
+describe("Delete task tests", function () {
+    beforeEach(function () {
+        return react_2.render(react_1["default"].createElement(deleteTask_component_1.DeleteTask, { item: TASK1, tasks: TASKLIST1, setTasks: function ( /*newTasks: Task[]*/) {
+                throw new Error("function not implemented");
+            } }));
+    });
+    test("There is a switch", function () {
+        var switchButton = react_2.screen.getByRole("switch");
+        expect(switchButton).toBeInTheDocument();
+    });
+    test("On switch click, there is a textbox and buttons: ", function () {
+        var switchButton = react_2.screen.getByRole("switch");
+        switchButton.click();
+        var deltask1 = react_2.screen.getByRole("button", {
+            name: /Delete Task and Leave Edit Mode/i
+        });
+        expect(react_2.screen.getByRole("textarea")).toBeInTheDocument();
+        expect(deltask1).toBeInTheDocument();
+    });
+    test("Clicking delete task deletes from the list and leaves edit mode", function () {
+        var switchButton = react_2.screen.getByRole("switch");
+        switchButton.click();
+        var delTask = react_2.screen.getByRole("button", {
+            name: /Delete Task and Leave Edit Mode/i
+        });
+        var inputUser = react_2.screen.getByRole("textarea");
+        user_event_1["default"].type(inputUser, "task test");
+        // on delete task button click
+        delTask.click();
+        expect(react_2.screen.getByRole("textarea")).toBeNull();
+        expect(react_2.screen.getByRole("button")).toBeNull();
+    });
+});

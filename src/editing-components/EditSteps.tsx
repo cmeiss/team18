31644117f53d53d13aac1afae5fa5ceb09a2d450
Will Sharp <sharpie@udel.edu>
2023-05-04 16:14:51 -0400

    Starting the EditSteps Component

---
+++ src/editing-components/EditSteps.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Form } from "react-bootstrap";
+
+interface stepsProps {
+    steps: string[];
+    setSteps: (newSteps: string[]) => void;
+}
+
+export function EditSteps({ steps, setSteps }: stepsProps) {
+    function updateSteps(event: React.ChangeEvent<HTMLInputElement>) {
+        const newStep = String(event.target.value);
+        setSteps([...steps, newStep]);
+    }
+    return (
+        <div>
+            <Form.Group controlId="form">
+                <Form.Label>Change Description:</Form.Label>
+                <Form.Control value={steps} onChange={updateSteps} />
+            </Form.Group>
+            <div>
+                To Indicate Different Steps leave an "$" symbol in between the
+                various steps
+            </div>
+        </div>
+    );
+}

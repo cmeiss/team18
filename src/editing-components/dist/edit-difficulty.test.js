92ca803dd3acb45cd36e460153d7e8a586b8839e
sydni wright <sydniw@udel.edu>
2023-05-06 18:14:02 -0400

    working in edit difficulty tests, deleted unnedeeded coments in another file

--- src/editing-components/dist/edit-difficulty.test.js
+++ src/editing-components/dist/edit-difficulty.test.js
@@ -11,4 +11,15 @@ describe("testing edit difficulty component", function () {
     beforeEach(function () {
         react_2.render(react_1["default"].createElement(edit_difficulty_1.EditDifficulty, null));
     });
+    test("component displays", function () {
+        var compDisplayed = react_2.screen.getByText("Change Difficulty: ");
+        expect(compDisplayed).toBeInTheDocument();
+    });
+    test("difficulty is updating", function () {
+        var chooseDifficulty = react_2.screen.getByLabelText("Change Difficulty");
+        react_2.fireEvent.click(chooseDifficulty);
+        var newDifficulty = react_2.screen.getByRole("option", { difficulty: 0 });
+        react_2.fireEvent.click(newDifficulty);
+        expect(newDifficulty).toBeInTheDocument();
+    });
 });
--- src/editing-components/edit-difficulty.test.tsx
+++ src/editing-components/edit-difficulty.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { EditDifficulty } from "./edit-difficulty";
 
 const difficulty = 100;
@@ -11,5 +11,16 @@ function setDifficulty(newDiff: number) {
 describe("testing edit difficulty component", () => {
     beforeEach(() => {
         render(<EditDifficulty></EditDifficulty>);
+    });
+    test("component displays", () => {
+        const compDisplayed = screen.getByText("Change Difficulty: ");
+        expect(compDisplayed).toBeInTheDocument();
+    });
+    test("difficulty is updating", () => {
+        const chooseDifficulty = screen.getByLabelText("Change Difficulty");
+        fireEvent.click(chooseDifficulty);
+        const newDifficulty = screen.getByRole("option", { difficulty: 0 });
+        fireEvent.click(newDifficulty);
+        expect(newDifficulty).toBeInTheDocument();
     });
 });

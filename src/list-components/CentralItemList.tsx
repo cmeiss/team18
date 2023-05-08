74d540724e2af7f5465a4d28087e62353992395b
Cornelia <cmeiss@udel.edu>
2023-05-08 16:35:51 -0400

    enabled sorting in central item list for super only, used same design as sorting in admin and user list

--- src/list-components/CentralItemList.tsx
+++ src/list-components/CentralItemList.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 //import { Button } from "react-bootstrap";
 import { DisplayTask } from "./DisplayTask";
 import { Task } from "../interfaces/task";
+import {
+    filter_by_alphabetical_order,
+    filter_by_difficulty,
+    filter_by_time_needed
+} from "./filterlists";
+import { Button, Col, Row } from "react-bootstrap";
 
 interface CentralItemProps {
     tasks: Task[];
@@ -11,11 +17,38 @@ interface CentralItemProps {
 }
 
 export function CentralItemList({ role, tasks, setTasks }: CentralItemProps) {
-    return (
-        <div className="central-list">
-            <h2> Sample Tasks </h2>
+    const [sorted, setSorted] = useState<boolean>(false); //indicated whether the adminList is sorted right now
+    const [alphabetic, setAlphabetic] = useState<boolean>(false);
+    const [byTime, setByTime] = useState<boolean>(false);
+    const [byDifficulty, setByDifficulty] = useState<boolean>(false);
+
+    function newCentralList(taskList: Task[]) {
+        return taskList.map((TASK: Task) => TASK);
+    }
+
+    function updateAlphabetic() {
+        setSorted(true);
+        setAlphabetic(true);
+        setByTime(false);
+        setByDifficulty(false);
+    }
+    function updateByTime() {
+        setSorted(true);
+        setAlphabetic(false);
+        setByTime(true);
+        setByDifficulty(false);
+    }
+    function updateByDifficulty() {
+        setSorted(true);
+        setAlphabetic(false);
+        setByTime(false);
+        setByDifficulty(true);
+    }
+
+    function displayList(taskList: Task[]): JSX.Element {
+        return (
             <div className="central-tasks">
-                {tasks.map((TASK: Task, index: number) => (
+                {taskList.map((TASK: Task, index: number) => (
                     <DisplayTask
                         key={index}
                         task={TASK}
@@ -25,9 +58,80 @@ export function CentralItemList({ role, tasks, setTasks }: CentralItemProps) {
                     ></DisplayTask>
                 ))}
             </div>
-            {console.log("Central Item List")}
-            {console.log(...tasks)}
-            {/*the purpose of this console.log statement is to display the changes in the tasks in the console because they are not showing in the displayed app right now*/}
-        </div>
-    );
+        );
+    }
+
+    function DisplayCorrectList(): JSX.Element {
+        //this function checks if we are displaying the adminList sorted or unsorted.
+        //If we display it unsorted, we directly pull it out of the central item list.
+        //If we display it sorted, we create a new sorted temporary list and display that.
+        if (!sorted) {
+            return displayList(newCentralList(tasks));
+        } else if (alphabetic) {
+            const SortedList = filter_by_alphabetical_order(
+                newCentralList(tasks)
+            );
+            return displayList(SortedList);
+        } else if (byTime) {
+            const SortedList = filter_by_time_needed(newCentralList(tasks));
+            return displayList(SortedList);
+        } else if (byDifficulty) {
+            const SortedList = filter_by_difficulty(newCentralList(tasks));
+            return displayList(SortedList);
+        } else {
+            return <div>AdminList failed to load.</div>;
+        }
+    }
+    if (role !== "Super") {
+        return (
+            <div className="central-list">
+                <h2> Sample Tasks </h2>
+                <div className="central-tasks">
+                    {tasks.map((TASK: Task, index: number) => (
+                        <DisplayTask
+                            key={index}
+                            task={TASK}
+                            tasks={tasks}
+                            updateTasks={setTasks}
+                            role={role}
+                        ></DisplayTask>
+                    ))}
+                </div>
+                {console.log("Central Item List")}
+                {console.log(...tasks)}
+                {/*the purpose of this console.log statement is to display the changes in the tasks in the console because they are not showing in the displayed app right now*/}
+            </div>
+        );
+    } else {
+        return (
+            <div className="central-list">
+                <Row>
+                    <Col>
+                        <h2> Central Item List </h2>
+                    </Col>
+                    <Col>
+                        <div className="Centralsort-dropdown">
+                            <button className="Centralsort-dropbtn">
+                                {/*eslint-disable-next-line prettier/prettier*/}
+                                Sort by ▾
+                            </button>
+                            <div className="Centralsort-options">
+                                <Button onClick={updateAlphabetic}>
+                                    Alphabetical{" "}
+                                </Button>
+                                <Button onClick={updateByDifficulty}>
+                                    Difficulty{" "}
+                                </Button>
+                                <Button onClick={updateByTime}>Time </Button>
+                                <Button onClick={() => setSorted(false)}>
+                                    Reset
+                                </Button>
+                            </div>
+                        </div>
+                    </Col>
+                </Row>
+                <DisplayCorrectList></DisplayCorrectList>
+            </div>
+        );
+    }
 }

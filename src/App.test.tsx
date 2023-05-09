d139ae58a191a19d12751bf275f40f8af9863aad
Cornelia <cmeiss@udel.edu>
2023-05-09 19:38:04 -0400

    found solution to jest problem but need to implement it on newest version of project to avoid merge conflicts

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -10,6 +10,9 @@ import { AdminList } from "./list-components/adminlist";
 import { DeleteTask } from "./list-components/deleteTask-component";
 import { AddTask } from "./list-components/addTask";
 import { EditTask } from "./editing-components/EditTask";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { renderWithDnd } from "./CustomRender";
 
 const testTask = {
     id: 1,
@@ -50,7 +53,16 @@ function setUsers(newUsers: User[]) {
 
 describe("Tesing Central Item List in App", () => {
     beforeEach(() => {
-        render(
+        // render(
+        //     <DndProvider backend={HTML5Backend}>
+        //         <CentralItemList
+        //             tasks={TASKS}
+        //             role={role1}
+        //             setTasks={setTasks}
+        //         ></CentralItemList>
+        //     </DndProvider>
+        // );
+        renderWithDnd(
             <CentralItemList
                 tasks={TASKS}
                 role={role1}
@@ -66,7 +78,7 @@ describe("Tesing Central Item List in App", () => {
 
 describe("Tesing User List in App", () => {
     beforeEach(() => {
-        render(
+        renderWithDnd(
             <UserList
                 users={users}
                 setUsers={setUsers}
@@ -82,7 +94,7 @@ describe("Tesing User List in App", () => {
         expect(listDisplayed).toBeInTheDocument();
     });
     test("list not seen by super", () => {
-        const { container } = render(
+        const { container } = renderWithDnd(
             <UserList
                 users={users}
                 setUsers={setUsers}
@@ -95,7 +107,7 @@ describe("Tesing User List in App", () => {
         expect(container).toBeEmptyDOMElement();
     });
     test("list not seen by admin", () => {
-        const { container } = render(
+        const { container } = renderWithDnd(
             <UserList
                 users={users}
                 setUsers={setUsers}
@@ -111,7 +123,7 @@ describe("Tesing User List in App", () => {
 
 describe("Tesing Admin List in App", () => {
     beforeEach(() => {
-        render(
+        renderWithDnd(
             <AdminList
                 users={users}
                 setUsers={setUsers}
@@ -122,7 +134,7 @@ describe("Tesing Admin List in App", () => {
         );
     });
     test("list is displayed", () => {
-        const listDisplayed = screen.getByText("Admin List");
+        const listDisplayed = screen.getByText("Pending List");
         expect(listDisplayed).toBeInTheDocument();
     });
     test("list not seen by super", () => {
@@ -138,7 +150,7 @@ describe("Tesing Admin List in App", () => {
         expect(container).toBeEmptyDOMElement();
     });
     test("list not seen by user", () => {
-        const { container } = render(
+        const { container } = renderWithDnd(
             <AdminList
                 users={users}
                 setUsers={setUsers}
@@ -152,7 +164,7 @@ describe("Tesing Admin List in App", () => {
 });
 describe("Testing Delete task in App", () => {
     beforeEach(() =>
-        render(
+        renderWithDnd(
             <DeleteTask
                 tasks={TASKS}
                 setTasks={function (): void {
---
+++ src/CustomRender.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export const renderWithDnd = (ui: {} | null | undefined) => {
+    // eslint-disable-next-line react/prop-types
+    // const Wrapper = ({ children }) => (
+    //     <DndProvider backend={HTML5Backend}>{children}</DndProvider>
+    // );
+
+    return render(<DndProvider backend={HTML5Backend}>{ui}</DndProvider>);
+};

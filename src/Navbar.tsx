88051dffcd05d95795a1d04236b76102a6a4b1cf
aaron oster <bigaaron@udel.edu>
2023-05-09 10:22:43 -0400

    styled nav bar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -3,16 +3,22 @@ import React from "react";
 
 const Navbar: React.FC = () => {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <a href="/">Home</a>
+        <nav className="navbar">
+            <ul className="navbar-list">
+                <li className="navbar-item">
+                    <a href="#" className="navbar-link">
+                        Home
+                    </a>
                 </li>
-                <li>
-                    <a href="/about">About</a>
+                <li className="navbar-item">
+                    <a href="#" className="navbar-link">
+                        About
+                    </a>
                 </li>
-                <li>
-                    <a href="/contact">Contact</a>
+                <li className="navbar-item">
+                    <a href="#" className="navbar-link">
+                        Contact
+                    </a>
                 </li>
             </ul>
         </nav>

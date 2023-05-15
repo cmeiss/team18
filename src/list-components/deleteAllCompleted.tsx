8557f534ac03b27b63f6fcb5b17b7d743177ff24
Will Sharp <sharpie@udel.edu>
2023-05-15 14:57:13 -0400

    Continuning to make changes to deleteAllCompleted

---
+++ src/list-components/deleteAllCompleted.tsx
@@ -0,0 +1,28 @@
+import { Button } from "react-bootstrap";
+import { Task } from "../interfaces/task";
+import React from "react";
+
+export interface delCompletedTaskProp {
+    tasks: Task[];
+    setTasks: (newTasks: Task[]) => void;
+}
+
+export function delelteAllCompleted(
+    tasksProp: delCompletedTaskProp
+): JSX.Element {
+    const newTasks = tasksProp.tasks.filter(
+        (task: Task): boolean => !task.status
+    );
+
+    function updatedTasks(tasks: Task[]) {
+        tasksProp.setTasks(
+            tasks.map((T: Task) => ({ ...T, steps: [...T.steps] }))
+        );
+    }
+
+    return (
+        <Button onClick={() => updatedTasks(newTasks)}>
+            Delete All Completed
+        </Button>
+    );
+}

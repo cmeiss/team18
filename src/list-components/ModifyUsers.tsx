bb35d05d5b1605538d7a4cabfe74528cba35855a
Cornelia <cmeiss@udel.edu>
2023-05-17 10:31:23 -0400

    edited addUsers to only take a certain user name once

--- src/list-components/ModifyUsers.tsx
+++ src/list-components/ModifyUsers.tsx
@@ -13,7 +13,8 @@ export function ModifyUsers(
     ChangeRoleProps: ChangeRoleProperties
 ): JSX.Element {
     const [editmode, seteditmode] = useState<boolean>(false); //whether the textbox will appear boolean
-    const [newUser, setNewUser] = useState<string>("User2"); //the value currently in the text box of edit mode
+    const [newUser, setNewUser] = useState<string>(""); //the value currently in the text box of edit mode
+    const [placeholder, setPlaceholder] = useState<string>("Enter User"); //holds the current placeholder text to ask for input
     //function that sets role based on the role clicked
     function updateEditMode(event: React.ChangeEvent<HTMLInputElement>) {
         seteditmode(event.target.checked);
@@ -23,11 +24,25 @@ export function ModifyUsers(
     }
 
     function AddUsersandEditMode() {
-        ChangeRoleProps.setRoles([
-            ...ChangeRoleProps.roles,
-            { name: newUser, userList: [] }
-        ]);
-        seteditmode(false);
+        const duplicate = ChangeRoleProps.roles.some(
+            (user: User) => String(user.name) === newUser
+        );
+        console.log("duplicate:");
+        console.log(duplicate);
+        if (!duplicate) {
+            ChangeRoleProps.setRoles([
+                ...ChangeRoleProps.roles,
+                { name: newUser, userList: [] }
+            ]);
+            seteditmode(false);
+            setNewUser("");
+            setPlaceholder("Enter User");
+        } else {
+            const newPH =
+                newUser + " already exists, please enter a different name";
+            setPlaceholder(newPH);
+            setNewUser("");
+        }
     }
     function DeleteUsersandEditMode() {
         ChangeRoleProps.setRoles(
@@ -56,6 +71,7 @@ export function ModifyUsers(
                         rows={3}
                         value={newUser}
                         onChange={updateUsers}
+                        placeholder={placeholder}
                     />
                 </Form.Group>
             ) : null}

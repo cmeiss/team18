1bc3ad20b2da72b44363999bf482ac1af1386ba9
Will Sharp <sharpie@udel.edu>
2023-05-18 21:03:34 -0400

    Adding second test for tasks

--- src/list-components/search.test.tsx
+++ src/list-components/search.test.tsx
@@ -2,7 +2,7 @@ import { search } from "./search";
 import { TASKS } from "../TASKS";
 
 describe("Search Component tests", () => {
-    test("Testing if the filter is working correctly", () => {
+    test("Testing the search by name component", () => {
         expect(search("Breakfast", TASKS)).toStrictEqual([TASKS[3]]);
         expect(search("Homework", TASKS)).toStrictEqual([TASKS[0]]);
     });
